ed970be760cd5059b32cd310393ada7c3183d402
zhutianlei24 <zhutianlei2b@gmail.com>
2023-10-27 17:49:05 +0200

    chanegs

--- src/trade/trade.service.ts
+++ src/trade/trade.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { GlobalService } from 'src/utils/gloabl.service';
 import { CreateTradeDto } from './dto/create-trade.dto';
 import * as zklink from 'zklink-js-sdk';
+import Web3 from 'web3';
+import { EthMessageSigner } from 'zklink-js-sdk/build/eth-message-signer';
+import { ethers } from 'ethers';
+import { Signer } from 'zklink-js-sdk/build/signer';
+import { OrderData, OrderMatchingEntries } from 'zklink-js-sdk';
 
 @Injectable()
 export class TradeService {
@@ -11,6 +16,16 @@ export class TradeService {
   }
 
   buyOrderMatching(createTradeDto: CreateTradeDto) {
+    const provider = new ethers.JsonRpcProvider(process.env.INFURA_ENDPOINT)
+
+    // Initialize the zkSigner and ethSigner for submitter
+    const zkSignerSubmitter = Signer.fromPrivateKey(Uint8Array.from(Array.from(process.env.ZKLINK_SUBMITTER_PRIVATEKEY).map(letter => letter.charCodeAt(0))));
+    const ethSignerSubmitter = new EthMessageSigner(new ethers.Wallet(process.env.ZKLINK_SUBMITTER_PRIVATEKEY, provider))
+
+    // Initialize the zkSigner and ethSigner for User
+    const zkSingerUser = Signer.fromPrivateKey(Uint8Array.from(Array.from(process.env.USER_PRIVATEKEY).map(letter => letter.charCodeAt(0))));
+    const ethSignerUser = new EthMessageSigner(new ethers.Wallet(process.env.USER_PRIVATEKEY, provider))
+
     if (createTradeDto.price < GlobalService.sellOrders[0].price)
     {
       // add this buy order to the buy order list and sort it with price desc
@@ -28,10 +43,53 @@ export class TradeService {
         if(residue <= 0) {
           break;
         }
-        if(buyPrice >= sellOrder.price) {
-        }
-      }
 
+        if(buyPrice >= sellOrder.price) {
+          // construct the order
+          const order_data_taker = {
+            type: 'order',
+            account_id: ???,
+            sub_account_id: 4,
+            slot: 1,
+            nonce: 1,
+            baseTokenId: 141,
+            quoteTokenId: 1,
+            price: sellOrder.price * 10 ** 18,
+            amount: sellOrder.amount * 10 ** 18,
+            is_sell: 1,
+            taker_fee_ratio: 255,
+            maker_fee_ratio:255
+          } as unknown as OrderData 
+
+          
+        const order_data_maker = {
+          type: 'order',
+          account_id: ???,
+          sub_account_id: 4,
+          slot: 1,
+          nonce: 1,
+          baseTokenId: 141,
+          quoteTokenId: 1,
+          price: sellOrder.price * 10 ** 18,
+          amount: sellOrder.amount * 10 ** 18,
+          is_sell: 0,
+          taker_fee_ratio: 255,
+          maker_fee_ratio:255
+        } as unknown as OrderData 
+
+        const order_matching = {
+          accountId: ???,
+          subAccountId: 4,
+          taker: order_data_taker,
+          maker: order_data_maker,
+          expectBaseAmount: sellOrder.amount * 10 ** 18,
+          expectQuoteAmount: sellOrder.price * 10 ** 18,
+          feeTokenId: 141,
+          feeTokenSymbol: 'WETH',
+          fee: 405000000000000,
+        } as unknown as OrderMatchingEntries 
+      } 
+      }
     }
   }
 
