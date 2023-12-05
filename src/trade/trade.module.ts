115f1c38ba626877ddbdcb5bc08077e878a66f38
zhutianlei24 <zhutianlei2b@gmail.com>
2023-12-05 12:11:57 +0100

    fix small bug

--- src/trade/trade.module.ts
+++ src/trade/trade.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TradeService } from './trade.service';
 import { TradeController } from './trade.controller';
+import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [HttpModule, CacheModule.register()],
   controllers: [TradeController],
   providers: [TradeService],
 })
